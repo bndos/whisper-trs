908ac69f8ad95223884b9cf74c75b4c5bb7c2d94
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2024-02-12 18:06:13 -0500

    cleaned up server

--- audio_player.py
+++ audio_player.py
@@ -1,9 +1,8 @@
-import socket
 import time
 
 import numpy as np
-import requests
 import pyaudio
+import requests
 import torch
 import whisperx
 from transformers.models.pop2piano.feature_extraction_pop2piano import librosa
@@ -38,69 +37,29 @@ def audio_bytes_to_np(data: bytes, to_mono: bool = True):
     return audio_array.astype(np.float32) / PCM_16BIT_MAX
 
 
-def play_stream():
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=TARGET_RATE, output=True)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        print(f"Connected by {addr}")
-        full_data = b""
-
-        while True:
-            data = b""
-            while len(data) < CHUNK:
-                packet = conn.recv(CHUNK - len(data))
-                if not packet:
-                    break
-                data += packet
-
-            if not data:
-                break
-
-            full_data += data
-            audio_array = audio_bytes_to_np(full_data).astype(np.float32)
-            audio_np_16k = librosa.resample(
-                audio_array, orig_sr=RATE, target_sr=TARGET_RATE
-            )
-
-            stream.write(audio_np_16k.tobytes())
-            trs = audio_model.transcribe(audio_np_16k, batch_size=8)
-            print(trs["segments"])
-
-            time.sleep(3)
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
 def fetch_audio_metadata(url):
     response = requests.get(url)
-    response.raise_for_status()  # Raise an error for bad responses
+    response.raise_for_status()
     return response.json()
+
 
 def main(audio_metadata_url, audio_stream_url):
     # Fetch audio metadata
     metadata = fetch_audio_metadata(audio_metadata_url)
 
-    rate = metadata['sample_rate']
-    chunk_size = metadata['chunk_size']
+    rate = metadata["sample_rate"]
+    chunk_size = metadata["chunk_size"]
 
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=TARGET_CHANNELS,
-                    rate=TARGET_RATE,
-                    output=True)
+    stream = p.open(
+        format=FORMAT, channels=TARGET_CHANNELS, rate=TARGET_RATE, output=True
+    )
 
-    # Stream and play the audio
     with requests.get(audio_stream_url, stream=True) as r:
         r.raise_for_status()
         full_data = b""
         data = b""
         for chunk in r.iter_content(chunk_size=chunk_size):
-            # If stereo, convert to mono by selecting one channel
             data += chunk
 
             if len(data) < chunk_size:
@@ -108,14 +67,22 @@ def main(audio_metadata_url, audio_stream_url):
 
             full_data += data
 
-            full_data_np = audio_bytes_to_np(full_data, to_mono=metadata['channels'] == 2)
-            full_data_np = librosa.resample(full_data_np, orig_sr=rate, target_sr=TARGET_RATE)
+            data_np = audio_bytes_to_np(data, to_mono=metadata["channels"] == 2)
+            data_np = librosa.resample(
+                data_np, orig_sr=rate, target_sr=TARGET_RATE
+            )
 
-            # Play audio chunk
+            full_data_np = audio_bytes_to_np(
+                full_data, to_mono=metadata["channels"] == 2
+            )
+            full_data_np = librosa.resample(
+                full_data_np, orig_sr=rate, target_sr=TARGET_RATE
+            )
+
             trs = audio_model.transcribe(full_data_np, batch_size=8)
             print(trs["segments"])
 
-            stream.write(full_data_np.tobytes())
+            stream.write(data_np.tobytes())
             time.sleep(2)
             data = b""
 
@@ -123,8 +90,8 @@ def main(audio_metadata_url, audio_stream_url):
     stream.close()
     p.terminate()
 
+
 if __name__ == "__main__":
-    # play_stream()
     file_name = "devenir-riche.wav"
     audio_metadata_url = f"http://{HOST}:{PORT}/audio/metadata/{file_name}"
     audio_stream_url = f"http://{HOST}:{PORT}/stream/audio?file_name={file_name}"
